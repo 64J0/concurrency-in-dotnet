44b38fc40e5af83cf8d83e60dabfa9f753798e29
64J0 <vinigaio97@gmail.com>
2023-10-30 20:09:49 -0300

    feat: add chapter 2 closure examples

---
+++ chapter-2/closure.fsx
@@ -0,0 +1,42 @@
+let example1 () =
+    let somePhrase = "is blue"
+    let lambdaFn = fun x -> $"{x} {somePhrase}"
+
+    let result = lambdaFn "The ocean"
+    printfn "result: %s" result // The ocean is blue
+
+example1 ()
+
+open System.Threading
+open System.Threading.Tasks
+
+// Doesn't work
+let example2 () =
+    for iteration in [ 1..10 ] do
+        Task.Factory.StartNew(fun () -> printfn "%A - %i" Thread.CurrentThread.ManagedThreadId, iteration)
+
+example2 ()
+
+let example3 () =
+    let displayNumber = fun n -> printfn "%i" n
+    let mutable i = 5
+    let taskOne = Task.Factory.StartNew(fun () -> displayNumber i)
+    i <- 7
+    let taskTwo = Task.Factory.StartNew(fun () -> displayNumber i)
+
+    Task.WaitAll(taskOne, taskTwo)
+// 7
+// 7
+
+example3 ()
+
+let example4 () =
+    let tasks = Array.zeroCreate<Task> 10
+
+    for index in [ 1..10 ] do
+        tasks.[index - 1] <- Task.Factory.StartNew(fun () -> printfn "%i" index)
+
+// One each line:
+// 1, 2, 4, 5, 6, 7, 8, 9, 10, 3
+
+example4 ()
